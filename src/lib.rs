915b114dfc53da6f01bbd8ada7c2d51d0c6beccc
Sebastian Köln <sebk@rynx.org>
2018-03-04 19:53:31 +0100

    separate simd part

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,28 @@
 #![feature(i128_type)]
 #![feature(const_fn)]
 #![feature(concat_idents)]
-#![cfg_attr(feature="impl_simd", feature(stdsimd))]
+#![cfg_attr(feature="simd", feature(stdsimd))]
 
 extern crate rand;
 extern crate tuple;
 
-#[cfg(feature="impl_simd")]
+#[cfg(feature="simd")]
 extern crate stdsimd;
+
+macro_rules! first_t {
+    ($A:ty, $B:tt) => ($A)
+}
+macro_rules! first_i {
+    ($A:ident, $B:tt) => ($A)
+}
+macro_rules! first_e {
+    ($a:expr, $b:tt) => ($a)
+}
 
 pub mod real;
 pub mod cast;
+#[cfg(feature="simd")]
+pub mod simd;
 
 pub use real::Real;
 pub use cast::*;
--- src/real.rs
+++ src/real.rs
@@ -3,7 +3,7 @@ use cast::Cast;
 use rand::{Rng};
 use rand::distributions::{IndependentSample, Range as Uniform};
 use std::fmt::Debug;
-use std::mem::transmute;
+use std::intrinsics::*;
 
 use tuple::*;
 //Float + NumCast + SampleRange + PartialOrd + Clone + Add + Debug
@@ -44,6 +44,9 @@ pub trait Real:
     fn ln(self) -> Self;
     */
 
+    fn floor(self) -> Self;
+    fn ceil(self) -> Self;
+    
     /// self * b + c
     #[inline]
     fn mul_add(self, b: Self, c: Self) -> Self {
@@ -141,7 +144,16 @@ macro_rules! impl_real {
 
             #[inline(always)]
             fn ln(self) -> Self { self.ln() }
-            */
+             */
+
+            #[inline(always)]
+            fn floor(self) -> Self {
+                $t::floor(self)
+            }
+            #[inline(always)]
+            fn ceil(self) -> Self {
+                $t::ceil(self)
+            }
             
             #[inline(always)]
             fn lt(self, rhs: Self) -> Self::Bool { self < rhs }
@@ -167,143 +179,6 @@ macro_rules! impl_real {
 }
 
 impl_real!(f32: fmaf32, f64: fmaf64);
-
-macro_rules! first_t {
-    ($A:ty, $B:tt) => ($A)
-}
-macro_rules! first_i {
-    ($A:ident, $B:tt) => ($A)
-}
-macro_rules! first_e {
-    ($a:expr, $b:tt) => ($a)
-}
-
-// highly unsafe macro
-macro_rules! call {
-    ($pre:ident, $name:ident, $post:ident ( $($arg:expr),* ) ) => (
-        unsafe {
-            use stdsimd::arch::x86_64::*;
-            transmute(concat_idents!($pre, $name, $post)( $( transmute($arg) ),* ))
-        }
-    )
-}
-
-macro_rules! impl_simd {
-    ($($simd:ident: $scalar:ident, $bool:ty, $pre:ident ~ $post:ident, $Tuple:ident($($idx:tt)*));*) => ( $(
-        impl Real for $simd {
-            const PI: Self = $simd::splat(::std::$scalar::consts::PI);
-            type Bool = $bool;
-            type Scalar = $scalar;
-            type Iterator = IntoElements<$Tuple<$(first_t!($scalar, $idx)),*>>;
-
-            #[inline(always)]
-            fn splat(s: Self::Scalar) -> Self {
-                $simd::splat(s)
-            }
-            
-            #[inline(always)]
-            fn values(self) -> Self::Iterator {
-                let mut arr = [$(first_e!(0., $idx)),*];
-                self.store(&mut arr, 0);
-                $Tuple::from(arr).into_elements()
-            }
-
-            #[inline(always)]
-            fn int(v: i16) -> Self { Self::splat($scalar::from(v)) }
-            #[inline]
-            fn float(f: f64) -> Self {
-                let f = f as $scalar;
-                Self::splat($scalar::from(f))
-            }
-            #[inline]
-            fn frac(nom: i16, denom: u16) -> Self {
-                Self::splat($scalar::from(nom) / $scalar::from(denom))
-            }
-
-            #[inline(always)]
-            fn wrap(self, at: Self, span: Self) -> Self {
-                Real::select(self - span, self, self.gt(at))
-            }
-            
-            fn uniform01<R: Rng>(rng: &mut R) -> Self {
-                let uniform01 = Uniform::new(0., 1.);
-                $simd::new($(first_e!(uniform01.ind_sample(rng), $idx)),*)
-            }
-
-            #[cfg(target_feature="fma")]
-            #[inline]
-            fn mul_add(self, b: Self, c: Self) -> Self {
-                Fma::mul_add(self, b, c)
-            }
-
-            #[inline(always)]
-            fn abs(self) -> Self {
-                Real::select(-self, self, self.le(Self::splat(0.0)))
-            }
-            #[inline(always)]
-            fn sqrt(self) -> Self {
-                call!($pre, sqrt, $post (self))
-            }
-
-            /*
-            #[inline(always)]
-            fn sin(self) -> Self { self.sin() }
-
-            #[inline(always)]
-            fn cos(self) -> Self { self.cos() }
-
-            #[inline(always)]
-            fn exp(self) -> Self { self.exp() }
-
-            #[inline(always)]
-            fn ln(self) -> Self { self.ln() }
-            */
-            
-            #[inline(always)]
-            fn min(self, other: Self) -> Self {
-                call!($pre, min, $post (self, other))
-            }
-            #[inline(always)]
-            fn max(self, other: Self) -> Self {
-                call!($pre, max, $post (self, other))
-            }
-            #[inline(always)]
-            fn lt(self, rhs: Self) -> Self::Bool { $simd::lt(self, rhs) }
-
-            #[inline(always)]
-            fn le(self, rhs: Self) -> Self::Bool { $simd::le(self, rhs) }
-
-            #[inline(always)]
-            fn gt(self, rhs: Self) -> Self::Bool { $simd::gt(self, rhs) }
-
-            #[inline(always)]
-            fn ge(self, rhs: Self) -> Self::Bool { $simd::ge(self, rhs) }
-
-            #[inline(always)]
-            fn eq(self, rhs: Self) -> Self::Bool { $simd::eq(self, rhs) }
-
-            #[inline(always)]
-            fn select(self, other: Self, cond: Self::Bool) -> Self {
-                call!($pre, blendv, $post (self, other, cond))
-            }
-        }
-    )* )
-}
-
-#[cfg(feature="impl_simd")]
-use stdsimd::simd::*;
-
-#[cfg(all(feature="impl_simd", target_feature = "mmx"))]
-impl_simd!(f32x4: f32, i32x4, _mm_ ~ _ps, T4(0 1 2 3));
-
-#[cfg(all(feature="impl_simd", target_feature = "sse2"))]
-impl_simd!(f64x2: f64, i64x2, _mm_ ~ _pd, T2(0 1));
-
-#[cfg(all(feature="impl_simd", target_feature = "avx"))]
-impl_simd!(
-    f32x8: f32, bool32fx8, _mm256_ ~ _ps, T8(0 1 2 3 4 5 6 7);
-    f64x4: f64, bool64fx4, _mm256_ ~ _pd, T4(0 1 2 3)
-);
 
 macro_rules! tuple_init {
     ($($Tuple:ident { $($T:ident . $t:ident . $idx:tt),* } )*) => ($(
@@ -360,6 +235,16 @@ macro_rules! tuple_init {
             #[inline]
             fn clamp(self, min: Self, max: Self) -> Self {
                 $Tuple( $(T::clamp(self.$idx, min.$idx, max.$idx),)* )
+            }
+
+            #[inline]
+            fn floor(self) -> Self {
+                $Tuple( $(T::floor(self.$idx),)* )
+            }
+
+            #[inline]
+            fn ceil(self) -> Self {
+                $Tuple( $(T::ceil(self.$idx),)* )
             }
 
             #[inline]
